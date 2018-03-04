a62c596f2da5519744b4d607ff752de5acbafd19
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-03-04 23:04:40 +1100

    Update providers and add LICENSE

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Burnex.Mixfile do
   def project do
     [
       app: :burnex,
-      version: "1.0.1",
+      version: "1.0.2",
       elixir: "~> 1.5",
       description: "Elixir burner email (temporary address) detector",
       start_permanent: Mix.env == :prod,
