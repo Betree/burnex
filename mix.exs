75b72ed35b505fd79f618f4dc9b896237e112bb6
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2018-08-05 19:48:53 +0200

    Bump version to 1.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Burnex.Mixfile do
   def project do
     [
       app: :burnex,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.6.6",
       description: "Elixir burner email (temporary address) detector",
       start_permanent: Mix.env == :prod,
